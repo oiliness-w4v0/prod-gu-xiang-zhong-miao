c4c9f2b0c87108795a350422b94aa2d285b6f329
czq040902 <16615812538@163.com>
2026-02-08 22:43:15 +0800

    change

--- src/counter.ts
+++ src/counter.ts
@@ -2,7 +2,7 @@ export function setupCounter(element: HTMLButtonElement) {
   let counter = 0
   const setCounter = (count: number) => {
     counter = count
-    element.innerHTML = `count 2222is ${counter}`
+    element.innerHTML = `count 23333222is ${counter}`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
